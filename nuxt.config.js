63b9096f1549f38493b0a3a3d23fbc46407899c4
rishigandhi <119334213+rishigandhi612@users.noreply.github.com>
2024-12-03 19:43:25 +0530

    deignore dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target:"static",
+  ssr:false,
+  generate:{
+    fallback:true
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - nuxt-todo',
